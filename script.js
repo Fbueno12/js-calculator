a515271ce59321713353203739973dfdd6975985
Felipe Bueno <felipebueno181@gmail.com>
2020-07-28 22:26:32 -0300

    finalizando calculadora sem tratamentos de erro

--- script.js
+++ script.js
@@ -4,18 +4,25 @@ function start() {
   const buttons = document.querySelectorAll('.btn');
   const visor = document.querySelector(".calc-visor");
   const reset = document.querySelector("#calc-reset");
+  const calculate = document.querySelector("#calculate");
 
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
-      if (visor.innerHTML == '0,00') {
+      if (visor.innerHTML == '0.00') {
         visor.innerHTML = '';
       }
-      visor.innerHTML = visor.innerHTML + button.innerHTML; 
+      if (button.innerHTML != '=') {
+        visor.innerHTML = visor.innerHTML + button.innerHTML; 
+      }
     });
   });
 
-  reset.addEventListener('click', () => {
-    visor.innerHTML = '0,00';
+  calculate.addEventListener('click', () => {
+    visor.innerHTML = eval(visor.innerHTML);
   });
-  
+
+  reset.addEventListener('click', () => {
+    visor.innerHTML = '0.00';
+  });
+
 }
